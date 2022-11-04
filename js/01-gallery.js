ce25049a312e8056104bef90b4107212ce3e62cf
GrzegorzKR <91796260+GrzegorzKR@users.noreply.github.com>
2022-11-04 21:51:14 +0100

    Task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,17 +1,39 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-// import * as basicLightbox from "basiclightbox";
 console.log(galleryItems);
 
-const instance = basicLightbox.create(`
-    <div class="modal">
-        <p>
-            Your first lightbox with just a few lines of code.
-            Yes, it's really that simple.
-        </p>
-    </div>
-`);
+const $gallery = document.querySelector("div.gallery");
 
-instance.show();
+galleryItems.forEach((element) => {
+  const $galleryItem = document.createElement("div");
+  $galleryItem.classList.add("gallery__item");
 
+  const $galleryLink = document.createElement("a");
+  $galleryLink.classList.add("gallery__link");
+  $galleryLink.setAttribute("href", element.original);
 
+  const $galleryImage = document.createElement("img");
+  $galleryImage.classList.add("gallery__image");
+  $galleryImage.src = element.preview;
+  $galleryImage.dataset.source = element.original;
+  $galleryImage.alt = element.description;
+
+  $gallery.append($galleryItem);
+  $galleryItem.append($galleryLink);
+  $galleryLink.append($galleryImage);
+});
+
+$gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.tagName === "IMG") {
+    const instance = basicLightbox.create(
+      `<img src = ${event.target.dataset.source}>`
+    );
+    instance.show();
+    document.addEventListener("keydown", (event) => {
+      if (event.key === "Escape") {
+        instance.close();
+      }
+    });
+  }
+});
