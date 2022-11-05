093f6c2472650d5681dc515ec7215928618bd6f1
GrzegorzKR <91796260+GrzegorzKR@users.noreply.github.com>
2022-11-05 15:50:19 +0100

    Task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+const $gallery = document.querySelector("ul.gallery");
+
+galleryItems.forEach((element) => {
+  const $galleryLink = document.createElement("a");
+  $galleryLink.classList.add("gallery__item");
+  $galleryLink.setAttribute("href", element.original);
+
+  const $galleryImage = document.createElement("img");
+  $galleryImage.classList.add("gallery__image");
+  $galleryImage.src = element.preview;
+  $galleryImage.alt = element.description;
+
+  $gallery.append($galleryLink);
+  $galleryLink.append($galleryImage);
+});
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: "250",
+});
